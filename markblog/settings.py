64a370a126affdc90a24bb6893f27724eaddeca6
jackeyGao <gaojunqi@outlook.com>
2016-04-08 15:42:29 +0800

    支持Docker

--- markblog/settings.py
+++ markblog/settings.py
@@ -137,6 +137,16 @@ USE_L10N = True
 USE_TZ = False
 
 
+STATICFILES_STORAGE = "qiniustorage.backends.QiniuStorage"
+DEFAULT_FILE_STORAGE = 'qiniustorage.backends.QiniuStorage'
+
+QINIU_ACCESS_KEY = '-9GvtvlzlYsJThtrNMVocrhcsh3lmOTAuY6aXEBT'
+QINIU_SECRET_KEY = 'l7fqBwgd-3M5ApcquLCFb-KKmLmNcIrlpQGJbBem'
+QINIU_BUCKET_NAME = 'privacy'
+#QINIU_BUCKET_DOMAIN = '7xlitg.com1.z0.glb.clouddn.com'
+QINIU_BUCKET_DOMAIN = 'dn-omem.qbox.me'
+QINIU_SECURE_URL = 'true'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
