b2946d4e3552ad9c86e1ac5eace6423a73ad75f4
jackeyGao <gaojunqi@outlook.com>
2016-04-11 13:01:52 +0800

    移除grappelli url 引用

--- markblog/urls.py
+++ markblog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'markblog.views.home', name='home'),
     url(r'^', include('markbook.urls')),
-    url(r'^grappelli/', include('grappelli.urls')), 
+    #url(r'^grappelli/', include('grappelli.urls')), 
     url(r'^admin/', include(admin.site.urls)),
 ]
